af908f56a8a48a6b969c77c9948693d779a5fceb
smchambers95 <smchambers95@gmail.com>
2017-04-09 13:32:37 +1200

    init commit
    kiwiisland code + keyboard input added

---
+++ src/nz/ac/aut/ense701/gameModel/InputHandler.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.ac.aut.ense701.gameModel;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Me
+ */
+public class InputHandler extends KeyAdapter{
+    private Game game;
+    private HashMap<String, Boolean> keyStates;
+    
+    public InputHandler(Game game){
+        this.game = game;
+        keyStates = new HashMap<String, Boolean>(){};
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        
+        switch (keyCode) {
+            case KeyEvent.VK_W:
+                if(!keyStates.containsKey("W") || !keyStates.get("W")){
+                    game.playerMove(MoveDirection.NORTH);
+                    keyStates.put("W", Boolean.TRUE);
+                }
+                break;
+            case KeyEvent.VK_S:
+                if(!keyStates.containsKey("S") || !keyStates.get("S")){
+                    game.playerMove(MoveDirection.SOUTH);
+                    keyStates.put("S", Boolean.TRUE);
+                }
+                break;
+            case KeyEvent.VK_A:
+                if(!keyStates.containsKey("A") || !keyStates.get("A")){
+                    game.playerMove(MoveDirection.WEST);
+                    keyStates.put("A", Boolean.TRUE);
+                }
+                break;
+            case KeyEvent.VK_D:
+                if(!keyStates.containsKey("D") || !keyStates.get("D")){
+                    game.playerMove(MoveDirection.EAST);
+                    keyStates.put("D", Boolean.TRUE);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    
+    @Override
+    public void keyReleased(KeyEvent e) {
+       int keyCode = e.getKeyCode();
+        
+        switch (keyCode) {
+            case KeyEvent.VK_W:
+                if(!keyStates.containsKey("W") || keyStates.get("W")){
+                    keyStates.put("W", Boolean.FALSE);
+                }
+                break;
+            case KeyEvent.VK_S:
+                if(!keyStates.containsKey("S") || keyStates.get("S")){
+                    keyStates.put("S", Boolean.FALSE);
+                }
+                break;
+            case KeyEvent.VK_A:
+                if(!keyStates.containsKey("A") || keyStates.get("A")){
+                    keyStates.put("A", Boolean.FALSE);
+                }
+                break;
+            case KeyEvent.VK_D:
+                if(!keyStates.containsKey("D") || keyStates.get("D")){
+                    keyStates.put("D", Boolean.FALSE);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
