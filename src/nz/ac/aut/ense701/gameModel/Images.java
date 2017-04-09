f7dbe09ce8f957d135d6967469938c41357a0ac5
smchambers95 <smchambers95@gmail.com>
2017-04-09 14:10:02 +1200

    Created resource manager
    Created resource manager. This commit is able to pull in the kiwiland
    textures, and store them in a HashMap. A sound HashMap has been created
    but have not got any sounds to pull in yet.

---
+++ src/nz/ac/aut/ense701/gameModel/Images.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.ac.aut.ense701.gameModel;
+
+/**
+ *
+ * @author Me
+ */
+public enum Images {
+    PLAYER,
+    KIWI,
+    POSSUM,
+    STOAT,
+    GRASS,
+    WATER,
+    SAND,
+    HOLE
+}
---
+++ src/nz/ac/aut/ense701/gameModel/ResourceManager.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.ac.aut.ense701.gameModel;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import javax.imageio.ImageIO;
+import sun.audio.AudioStream;
+
+/**
+ *
+ * @author Sean 
+ */
+public class ResourceManager {
+    HashMap<Images,BufferedImage> imageMap;
+    HashMap<Images,AudioStream> soundMap;
+    BufferedImage img;
+    
+    public ResourceManager() throws IOException
+    {
+        imageMap  = new HashMap();
+        soundMap = new HashMap();
+        
+        //Load in images first
+        //Load Player, and fauna sprites.
+        //Player
+        img = ImageIO.read(new File("textures/playerSprite.jpg"));
+        imageMap.put(Images.PLAYER,img);
+        //Kiwi
+        img = ImageIO.read(new File("textures/kiwiSprite.png"));
+        imageMap.put(Images.KIWI,img);
+        //Possum
+        img = ImageIO.read(new File("textures/possumSprite.gif"));
+        imageMap.put(Images.POSSUM,img);
+        //Stoat
+        img = ImageIO.read(new File("textures/stoatSprite.png"));
+        imageMap.put(Images.STOAT,img);
+        //Textures
+        //Grass
+        img = ImageIO.read(new File("textures/grassTexture.jpg"));
+        imageMap.put(Images.GRASS,img);
+        //Water
+        img = ImageIO.read(new File("textures/waterTexture.jpg"));
+        imageMap.put(Images.WATER,img);
+        //Sand
+        img = ImageIO.read(new File("textures/sandTexture.jpg"));
+        imageMap.put(Images.SAND,img);
+        //Hole
+        img = ImageIO.read(new File("textures/holeTexture.png"));
+        imageMap.put(Images.HOLE,img);
+        
+        //Load sound assets    
+    }
+    
+    
+}
